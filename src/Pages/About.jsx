0ee17fa8d4fe75b984bda94c31e0916cd419ab62
weahotis <weahotis9@gmail.com>
2022-08-15 07:37:33 -0700

    Latest Commit

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <h1 className='text-6xl'>About</h1>
+      <h1 className='text-6xl text-center'>About Me</h1>
+      <h4 className='text-center'>
+        I am Otis Weah Jr. I am currently Practicing React Js and this is my first Project.
+      </h4>
     </div>
   )
 }
